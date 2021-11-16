4a150bc4eb268a704e329b38d7e80b49531807ad
Joshua Padgett <jpadgett@jpadgett-a01-2.local>
2021-11-15 20:26:48 -0800

    Refactor for solc 0.8.9 Bump version to 1.1.0

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const { abi, evm } = require('./compile');
 
 const provider = new HDWalletProvider(
   process.env.ETH_MNEMONIC,
@@ -13,8 +13,8 @@ const deploy = async () => {
 
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
     .send({ from: accounts[0], gas: '1000000' });
 
   console.log('Contract deployed to', result.options.address);
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,18 +2,16 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require('../compile');
+const { abi, evm } = require('../compile');
 
 let accounts;
 let inbox;
 
 beforeEach(async () => {
-  // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
 
-  // Use one of those accounts to deploy the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
     .send({ from: accounts[0], gas: '1000000' });
 });
 
