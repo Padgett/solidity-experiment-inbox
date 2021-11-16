a7994389083867c7b64bbc56ae1f7583cb9fba7f
Joshua Padgett <jpadgett@jpadgett-a01-2.local>
2021-11-15 19:55:59 -0800

    Add contract deployment

--- deploy.js
+++ deploy.js
@@ -1,1 +1,23 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  process.env.ETH_MNEMONIC,
+  process.env.INFURA_ENDPOINT
+)
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+}
+deploy();
